935bbdbf771ccf678dd2de37c59f33d304662c65
Jajukym <jajukym@gmail.com>
2019-01-31 16:48:45 -0700

    organization

--- python3.6/PIP_GEN_id-0/PIP_GEN_id-0_launcher.bat
+++
@@ -1,2 +0,0 @@
-cd C:\Users\bryan.lee\Documents\GitHub\hello-world\python3.6\PIP_GEN_id-0
-python PIP_GEN_id-0.py
--- python3.6/PIP_GEN_id-0/README.md
+++
@@ -1,3 +0,0 @@
-# hello-world
-In the beginning,
-I created my first github repository/branch.
